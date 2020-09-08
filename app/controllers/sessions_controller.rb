d575f0f520587838de13e0ab8e6db20c2a1a3617
rumikoacopa <rumikorios@gmail.com>
2020-09-07 22:11:02 -0400

    added sessions controller to be able to authenticate user login/signup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,22 @@
 class SessionsController < ApplicationController
-    get '/login' do 
-      erb :'/sessions/login'
+    get '/login' do #purpose of the route is to show/render login page aka login form
+      erb :'/sessions/login' #need to mount controller in config.ru
     end
   
     post '/login' do 
-  
+        user = User.find_by_email(params[:email])  # find the user by their user email:
+        if user && user.authenticate(params[:password]) # if they typed in the right password 
+            session[:id] = user.id                 #then log them in, if not show them the form again
+            redirect "/"
+          else 
+            @error = "Incorrect email or password"
+            erb :'/sessions/login'
+        end
     end
-  end
+    delete '/logout' do
+        session.clear
+        redirect "/"
+      end
+end
+   
+    
