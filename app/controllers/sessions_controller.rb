1fa2d7fe093fda7088462e94b19d9e0abbfbecf0
rumikoacopa <rumikorios@gmail.com>
2020-09-08 19:34:29 -0400

    changed user variable , added user interpolation w/id

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,20 @@
 class SessionsController < ApplicationController
-    get '/login' do #purpose of the route is to show/render login page aka login form
+    get '/login' do #purpose of the route:to show/render login page aka login form
       erb :'/sessions/login' #need to mount controller in config.ru
     end
+    
   
-    post '/login' do 
-        user = User.find_by_email(params[:email])  # find the user by their user email:
-        if user && user.authenticate(params[:password]) # if they typed in the right password 
-            session[:id] = user.id                 #then log them in, if not show them the form again
-            redirect "/"
+    post '/login' do #purpose to receive Login Form
+        @user = User.find_by(email: params[:email])  # find the user by their user email: & log them in
+        if @user && @user.authenticate(params[:password]) # verify user, 
+            session[:id] = @user.id                 #then log them in, if not show them the form again
+            redirect "users/#{@user.id}"                            #authenticate and redirect to users landing page
           else 
             @error = "Incorrect email or password"
             erb :'/sessions/login'
         end
     end
+    
     delete '/logout' do
         session.clear
         redirect "/"
