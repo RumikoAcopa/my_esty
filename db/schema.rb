ddb0f7ac341e95927a6040feb18a017b13b5dc45
rumikoacopa <rumikorios@gmail.com>
2020-09-09 01:08:42 -0400

    added notes

--- db/schema.rb
+++ db/schema.rb
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 20200907221053) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "password_digest"
+    t.string   "password_digest" #b/c using bcrypt and has_secure_password
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
