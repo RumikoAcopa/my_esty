704b467b64c11a55279f4848548508f721082313
rumikoacopa <rumikorios@gmail.com>
2020-09-09 01:55:40 -0400

    uncommented/commented out my email pw for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,5 @@
 #Here is where I will create some seed data to work w/and test associations
-#User.create(name: "Rumiko", email: "Rumiko@Rumi.com", password: "password")
+#User.create(name: "Rumiko", email: "Rumiko@Rumi.com", password: "Skittles")
 
 #create an account
 #use AR to pre-associate data
-#kira.account.create(birthdate: "August 30, 2005")
