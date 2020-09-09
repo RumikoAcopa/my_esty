a55eb8f1ef60f70363749b8558469232cdf6f096
rumikoacopa <rumikorios@gmail.com>
2020-09-09 01:00:37 -0400

    modified post users route, signup route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,32 +7,47 @@ class UsersController < ApplicationController
 
   # GET: /users/new
   get "/users/new" do
+    
     erb :"/users/new.html"
   end
 
   # POST: /users
-  #post "/users" do
-  #  redirect "/users"
-  #end
+  post "/users" do #here is where we will create a new user
+    @user = User.create(params)
+      if @user.save
+      session[:user_id] = @user.id  #logs the user in once they sign up 
+                              #then log them in, if not show them the form again
+                              #and persist the new user to db
+      redirect "/users/#{@user.id}" #params will look like this: "name"=>"Rumiko"
+      else
+        #it would be nice to include a message telling them what is wrong
+      redirect '/signup'
+    end
+  end
+
 
   get '/signup' do
-    erb :'/users/new'
+    erb :'/signup'
   end
 
   post '/signup' do
-    @user = User.new(name: params[:name], email: params[:email], password: params[:password])
-    if @user.save
-      session[:id] = @user.id
-      redirect "/sessions/login"
-    else
-      @error = "Think Again, Try harder this time!"
-      erb :'/users/new.html'
-    end
+    #@user = User.new(name: params[:name], email: params[:email], password: params[:password])
+    #if @user.save
+    #session[:user_id] = @user.id
+    # redirect "/sessions/login"
+    #else
+    #  @error = "Think Again!"
+    #  erb :'/users/new
+    #end
   end
 
   # GET: /users/5
   get '/users/:id' do #this will be the users show route
-    erb :"/users/show.html"
+    #first create variable
+    @user = User.find_by(id: params[:id])
+      #whats the act of logging someone in w/bcrypt
+
+    #erb :'/users/show'
   end
 
   # GET: /users/5/edit
@@ -48,5 +63,10 @@ class UsersController < ApplicationController
   # DELETE: /users/5/delete
   delete "/users/:id/delete" do
     redirect "/users"
+  end
+  private
+  def validate_input 
+    params[:name] != "" && params[:email] != "" &&
+    params[:password] != "" 
   end
 end
