cd8db5df98b7583235b250193fdc248c3d3b2312
rumikoacopa <rumikorios@gmail.com>
2020-09-09 21:15:07 -0400

    saved commented out info just in case

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,3 +3,11 @@ class User < ActiveRecord::Base
     has_secure_password
     has_many :account
 end
+#<label for="name">Cleanser</label>
+#<input type="text" name="name" id="name" />
+#<option value="Oil">Oil</option>
+#<label for="name">Size</label>
+#<input type="text" name="size" id="size" />
+#<label for="name">description</label>
+#<input type="text" name="description" id="description" />
+#<input type="submit" name="submit" value="Create Product" />
