9d53c63246997679c8e8f622e0b1908249d08925
rumikoacopa <rumikorios@gmail.com>
2020-09-12 18:10:51 -0400

    added validates name, email, and password attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,8 @@
 class User < ActiveRecord::Base 
     #now I have a ruby class that represents users
     has_secure_password
-    has_many :account
+    validates :name, presence: true
+    validates :email, presence: true
+    validates :password, presence: true
+    has_many :products
 end
-#<label for="name">Cleanser</label>
-#<input type="text" name="name" id="name" />
-#<option value="Oil">Oil</option>
-#<label for="name">Size</label>
-#<input type="text" name="size" id="size" />
-#<label for="name">description</label>
-#<input type="text" name="description" id="description" />
-#<input type="submit" name="submit" value="Create Product" />
